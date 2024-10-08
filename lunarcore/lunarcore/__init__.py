4ea26d6e4e4f5da3782a67ed552c366293870747
Danilo Miranda Gusicuma <danilo.m.gusicuma@gmail.com>
2024-10-08 16:04:21 -0300

    shared env file

---
+++ lunarcore/lunarcore/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: Copyright © 2024 Lunarbase (https://lunarbase.ai/) <contact@lunarbase.ai>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+import os.path
+from pathlib import Path
+
+from lunarcore.config import LunarConfig
+from lunarcore.core.persistence import PersistenceLayer
+from lunarcore.errors import ConfigFileIsMissing
+from lunarcore.utils import get_config
+
+ENV = f"{Path(__file__).parent.parent.parent.as_posix()}/.env"
+GLOBAL_CONFIG = None
+if os.path.isfile(ENV):
+    GLOBAL_CONFIG = get_config(settings_file_path=ENV)
+if GLOBAL_CONFIG is None:
+    raise ConfigFileIsMissing(ENV)
