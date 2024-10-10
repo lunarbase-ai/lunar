52f4da92834266b57ca5bffd41818ed1cf09a3af
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-10-10 11:17:09 -0300

    get all workflow paths and dirs from persistence layer

--- lunarcore/lunarcore/core/controllers/notebook_controller/__init__.py
+++ lunarcore/lunarcore/core/controllers/notebook_controller/__init__.py
@@ -3,6 +3,9 @@ from lunarcore.config import LunarConfig
 from lunarcore.utils import get_config
 from lunarcore.core.persistence_layer import PersistenceLayer
 from lunarcore.core.data_models import WorkflowModel
+from lunarcore.utils import setup_logger
+
+logger = setup_logger("notebook-controller")
 
 class NotebookController:
     def __init__(self, config: Union[str, Dict, LunarConfig]):
@@ -16,10 +19,20 @@ class NotebookController:
 
     async def save(self, workflow: WorkflowModel, user_id: str):
         workflow = WorkflowModel.model_validate(workflow)
-
         workflow_dag = workflow.get_dag()
+        workflow_venv_dir = self._persistence_layer.get_workflow_venv(
+            user_id=user_id, workflow_id=workflow.id
+        )
+        workflow_path = self._persistence_layer.get_user_workflow_path(
+                workflow_id=workflow.id, user_id=user_id
+            )
+        user_env_path = self._persistence_layer.get_user_environment_path(user_id)
 
+        logger.info(
+            self._persistence_layer.get_user_workflow_path(
+                workflow_id=workflow.id, user_id=user_id
+            )
+        )
         return {
             "workflow": workflow,
-            "dag": workflow_dag,
         }
