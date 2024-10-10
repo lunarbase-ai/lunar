b84b58a0b048ce9f6615ecc20a4192bacfdd5e3d
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-10-10 10:07:16 -0300

    notebook controller

--- lunarcore/lunarcore/api/__init__.py
+++ lunarcore/lunarcore/api/__init__.py
@@ -38,6 +38,7 @@ from lunarcore.core.controllers.code_completion_controller import (
 from lunarcore.core.controllers.file_controller import FileController
 from lunarcore.core.controllers.report_controller import ReportController
 from lunarcore.core.controllers.demo_controller import DemoController
+from lunarcore.core.controllers.notebook_controller import NotebookController
 from lunarcore.errors import ComponentError
 from lunarcore.core.typings.report import ReportSchema
 from lunarcore.core.data_models import (
@@ -78,6 +79,7 @@ async def app_startup():
     context.report_controller = ReportController(context.main_config)
     context.file_controller = FileController(context.main_config)
     context.code_completion_controller = CodeCompletionController(context.main_config)
+    context.notebook_controller = NotebookController(context.main_config)
 
     await context.component_api.index_global()
 
@@ -183,7 +185,7 @@ async def save_workflow_to_notebook(
 ):
     try:
         workflow = await context.workflow_api.get_by_id(workflow_id, user_id)
-        return workflow
+        return await context.notebook_controller.save(workflow, user_id)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
---
+++ lunarcore/lunarcore/core/controllers/notebook_controller/__init__.py
@@ -0,0 +1,24 @@
+from typing import Union, Dict
+from lunarcore.config import LunarConfig
+from lunarcore.utils import get_config
+from lunarcore.core.persistence_layer import PersistenceLayer
+from lunarcore.core.data_models import WorkflowModel
+
+class NotebookController:
+    def __init__(self, config: Union[str, Dict, LunarConfig]):
+        self._config = config
+        if isinstance(self._config, str):
+            self._config = get_config(settings_file_path=config)
+        elif isinstance(self._config, dict):
+            self._config = LunarConfig.parse_obj(config)
+
+        self._persistence_layer = PersistenceLayer(config=self._config)
+
+    async def save(self, workflow: WorkflowModel, user_id: str):
+        workflow = WorkflowModel.model_validate(workflow)
+
+        return {
+            "workflow": workflow,
+            "dag": workflow.get_dag(),
+            "user_id": user_id
+        }
