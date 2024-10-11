a7c9ffc1504c7c50c002e5eca558dfe30c0cd860
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2024-10-11 14:11:34 -0300

    first pass on full orchestration

--- lunarcore/lunarcore/core/controllers/notebook_controller/workflow_notebook_generator.py
+++ lunarcore/lunarcore/core/controllers/notebook_controller/workflow_notebook_generator.py
@@ -1,3 +1,4 @@
+from collections import deque
 from lunarcore.core.data_models import WorkflowModel, ComponentModel
 from typing import List
 import nbformat
@@ -5,6 +6,7 @@ from nbformat.notebooknode import NotebookNode
 from lunarcore.utils import setup_logger
 import json
 import re
+import networkx as nx
 
 logger = setup_logger("notebook-generator")
 
@@ -16,30 +18,18 @@ class WorkflowNotebookGenerator:
         components: List[ComponentModel] = workflow.components_ordered()
         notebook = self._start_new_notebook()
 
-
         title_markdown_cell = self._generate_title_cell(workflow)
         imports_code_cell = self._generate_imports_cell(components)
         component_instances_code_cell = self._generate_component_instances_cell(components)
-
-
-        # tasks = {comp.label: comp for comp in workflow.components}
-        # promises = {comp.label: dict() for comp in workflow.components}
-
-        # logger.info(f"Promises: {promises}")
-
-        # logger.info(f"Input 2: {tasks['TEXTINPUT-2']}") 
-        # logger.info("\n") 
-        # logger.info(f"Input 1: {tasks['TEXTINPUT-1']}")  
-        # logger.info("\n") 
-        # logger.info(f"Input 1: {tasks['TEXTINPUT-0']}")  
-        # dag = workflow.get_dag()say hi to {name}.
-        # running_queue = deque(list(dag.nodes))
-
+        orchestration_cells = self._generate_orchestration_cells(workflow)
+        output_cell = self._generate_output_cell(components)
         
         notebook.cells.extend([
             title_markdown_cell, 
             imports_code_cell, 
-            component_instances_code_cell
+            component_instances_code_cell,
+            *orchestration_cells,
+            output_cell
         ])
 
         return notebook
@@ -77,3 +67,65 @@ class WorkflowNotebookGenerator:
     
     def _format_as_python_variable(self, component_label: str) -> str:
         return re.sub(r'\W|^(?=\d)', '_', component_label).lower()
+    
+    # @TODO: Refactor this method to make it more robust and propper handle the orchestration. This is just a starting point. Its current implementation is not complete.
+    def _generate_orchestration_cells(self, workflow: WorkflowModel) -> List[NotebookNode]:
+        tasks = {comp.label: comp for comp in workflow.components}
+
+        dag = workflow.get_dag()
+        running_queue = deque(nx.topological_sort(dag))
+
+        dependencies = workflow.dependencies
+
+        orchestration_cells = []
+        while len(running_queue) > 0:
+            next_task = running_queue.popleft()
+            task = tasks[next_task]
+
+            instance_var_name = self._format_as_python_variable(task.label)
+            instance_result_var_name = f"{instance_var_name}_result"
+
+            component_inputs = []
+
+            input_dependencies = self._get_component_input_dependencies(dependencies, task.label)
+            for input in task.inputs:
+                key = input.key
+                value = input.value
+                template_variables = input.template_variables
+
+                if len(input_dependencies) > 0:
+                    for dependency in input_dependencies:
+                        if key == dependency.component_input_key:
+                            if dependency.template_variable_key is not None and dependency.template_variable_key in template_variables:
+                                template_variables[dependency.template_variable_key] = f"{self._format_as_python_variable(dependency.source_label)}_result"
+                                mapped_variables = {key.split('.')[-1]: value for key, value in template_variables.items()}
+                                value = value.format(**{key: f'{{{value}}}' for key, value in mapped_variables.items()})
+                                value = f'f"{value}"'
+
+                            else:
+                                value = f"{self._format_as_python_variable(dependency.source_label)}_result"
+                else:
+                    value = f'"{value}"'
+
+                component_inputs.append(f"{key}={value}")
+
+            run_call = f"{instance_result_var_name} = {instance_var_name}.run({', '.join(component_inputs)})"
+
+            orchestration_cells.append(nbformat.v4.new_code_cell(run_call))
+
+        return orchestration_cells
+    
+    def _get_component_input_dependencies(self, dependencies, task_label):
+        input_dependencies = []
+        for dependency in dependencies:
+            target_label = dependency.target_label
+            if target_label == task_label:
+                input_dependencies.append(dependency)
+                
+        return input_dependencies
+    
+    def _generate_output_cell(self, components: List[ComponentModel]) -> NotebookNode:
+        last_component = components[-1]
+        var_name = self._format_as_python_variable(last_component.label)
+
+        return nbformat.v4.new_code_cell(f"print({var_name}_result)")
