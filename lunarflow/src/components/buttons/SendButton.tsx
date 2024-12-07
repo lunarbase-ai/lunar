fa59b3766964532b0a12e15832a0689fd5b3a6d5
Danilo Miranda Gusicuma <danilo.m.gusicuma@gmail.com>
2024-12-07 11:53:57 -0300

    minor fix

--- lunarflow/src/components/buttons/SendButton.tsx
+++ lunarflow/src/components/buttons/SendButton.tsx
@@ -3,7 +3,7 @@
 import { SendOutlined } from "@ant-design/icons"
 import { ChatRequestOptions } from "ai"
 import { Button, Spin } from "antd"
-import React, { useState } from "react"
+import React from "react"
 
 interface SendButtonProps {
   value: string
@@ -17,17 +17,20 @@ const SendButton: React.FC<SendButtonProps> = ({ value, onSubmit, loading }) => 
 
   const size = value === '' && !loading ? 0 : 54
 
-  const handleSubmit = async () => {
-    await onSubmit()
-  }
-
   return <Button
     type='primary'
     shape='circle'
-    onClick={handleSubmit}
+    onClick={onSubmit}
     disabled={loading}
     icon={loading ? <Spin /> : <SendOutlined style={{ fontSize: size === 0 ? 0 : `18px` }} />}
-    style={{ width: size, height: size, marginLeft: size === 0 ? 0 : 8, minWidth: 0, padding: size === 0 ? 0 : '4px 0', border: size === 0 ? 0 : 1 }}
+    style={{
+      width: size,
+      height: size,
+      marginLeft: size === 0 ? 0 : 8,
+      minWidth: 0,
+      padding: size === 0 ? 0 : '4px 0',
+      border: size === 0 ? 0 : 1
+    }}
   />
 }
 
