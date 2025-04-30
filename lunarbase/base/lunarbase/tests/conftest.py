0a7fa77c7af8d81bf4d035009e0065acc86c0527
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2025-04-30 10:13:27 -0300

    local_file_datasources_test

--- lunarbase/base/lunarbase/tests/conftest.py
+++ lunarbase/base/lunarbase/tests/conftest.py
@@ -9,6 +9,19 @@ from lunarbase.controllers.datasource_controller import DatasourceController
 from lunarbase.controllers.workflow_controller import WorkflowController
 
 
+
+@pytest.fixture(scope="function", autouse=True)
+def startup():
+    LUNAR_CONTEXT.lunar_registry.persistence_layer.init_user_profile(
+        LUNAR_CONTEXT.lunar_config.DEFAULT_USER_TEST_PROFILE
+    )
+
+    yield
+
+    LUNAR_CONTEXT.lunar_registry.persistence_layer.delete_user_profile(
+        LUNAR_CONTEXT.lunar_config.DEFAULT_USER_TEST_PROFILE
+    )
+
 @pytest.fixture
 def workflow_controller():
     return WorkflowController(config=LUNAR_CONTEXT.lunar_config)
@@ -20,41 +33,9 @@ def component_controller():
 
 
 @pytest.fixture
-def uploaded_text_file():
-    filename = "lunar_uploaded_file_fixture.txt"
-    basepath = LUNAR_CONTEXT.lunar_registry.persistence_layer.get_user_file_root(
-        LUNAR_CONTEXT.lunar_config.DEFAULT_USER_PROFILE
-    )
-    filepath = Path(basepath, filename)
-    with open(filepath, "w") as f:
-        f.write("LUNAR")
-
-    yield UploadFile(file=filepath.open("rb"), filename=filename)
-
-    Path.unlink(filepath, missing_ok=True)
-
-
-@pytest.fixture
 def datasource_controller():
     return DatasourceController(config=LUNAR_CONTEXT.lunar_config)
 
-
-@pytest.fixture
-def local_file_datasource(datasource_controller, uploaded_text_file):
-    datasource = datasource_controller.create_datasource(
-        user_id=datasource_controller.config.DEFAULT_USER_PROFILE,
-        datasource={
-            "name": "Local file test datasource",
-            "type": "LOCAL_FILE",
-            "connection_attributes": {"file_name": uploaded_text_file.filename},
-        },
-    )
-    yield datasource
-
-    datasource_controller.delete_datasource(
-        user_id=datasource_controller.config.DEFAULT_USER_PROFILE,
-        datasource_id=datasource.id,
-    )
 
 @pytest.fixture
 def sparql_datasource(datasource_controller):
