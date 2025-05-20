6e18f0226f61e90b5feb3eda674fd05f0b1c3d03
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2025-05-20 10:48:17 -0300

    better organize test of datasource repository

---
+++ lunarbase/base/lunarbase/domains/datasources/repositories/tests/test_local_files_datasource_repository/conftest.py
@@ -0,0 +1,16 @@
+import pytest 
+from lunarbase.persistence.connections.local_files_storage_connection import LocalFilesStorageConnection
+from lunarbase.persistence.resolvers.local_files_path_resolver import LocalFilesPathResolver
+from lunarbase.domains.datasources.repositories.local_files_datasource_repository import LocalFilesDataSourceRepository
+
+@pytest.fixture
+def connection(config):
+    return LocalFilesStorageConnection(config)
+
+@pytest.fixture
+def path_resolver(config, connection):
+    return LocalFilesPathResolver(connection, config)
+
+@pytest.fixture
+def datasource_repository(config, connection, path_resolver):
+    return LocalFilesDataSourceRepository(connection, config, path_resolver) 
