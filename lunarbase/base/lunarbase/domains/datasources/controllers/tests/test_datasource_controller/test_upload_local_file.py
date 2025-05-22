23588b9ad7609c789b2b3010f5f4b3fc01b5a8d6
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2025-05-22 11:43:35 -0300

    test upload file fix

--- lunarbase/base/lunarbase/domains/datasources/controllers/tests/test_datasource_controller/test_upload_local_file.py
+++ lunarbase/base/lunarbase/domains/datasources/controllers/tests/test_datasource_controller/test_upload_local_file.py
@@ -66,8 +66,9 @@ class TestUploadLocalFile:
             mock_datasource_controller.file_storage_connection.delete.assert_called_once_with("/user/root/test.zip")
             
             assert len(mock_datasource.connection_attributes.files) == 2
-            assert mock_datasource.connection_attributes.files[0].file_name == "/user/root/extracted1.txt"
-            assert mock_datasource.connection_attributes.files[1].file_name == "/user/root/extracted2.txt"
+            file_names = {file.file_name for file in mock_datasource.connection_attributes.files}
+            assert "/user/root/extracted1.txt" in file_names
+            assert "/user/root/extracted2.txt" in file_names
             assert result == datasource_id
 
     def test_upload_local_file_invalid_datasource_type(self, mock_datasource_controller, config):
