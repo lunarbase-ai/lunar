8cc5c79ad7a866a181cbb2370f36da14e3e1ab92
Pedro Rocha <pedro.diniz.rocha@gmail.com>
2025-05-22 08:35:39 -0300

    fixing after rebase

--- lunarbase/base/lunarbase/domains/workflow/controllers/tests/test_workflow_controller/test_tmp_save.py
+++ lunarbase/base/lunarbase/domains/workflow/controllers/tests/test_workflow_controller/test_tmp_save.py
@@ -7,18 +7,6 @@ from lunarbase.modeling.data_models import WorkflowModel
 import pytest
 
 class TestTmpSave:
-    def test_returns_temporary_saved_workflow(self, controller, config):
-        user_id = config.DEFAULT_USER_TEST_PROFILE
-        workflow = WorkflowModel(
-            name="Test Workflow",
-            description="A test workflow",
-            id=str(uuid.uuid4()),
-        )
-        saved_workflow = controller.tmp_save(workflow, user_id)
-        assert saved_workflow.id == workflow.id
-        assert saved_workflow.name == workflow.name
-        assert saved_workflow.description == workflow.description
-
     def test_saves_workflow_in_tmp_path(self, controller, config):
         user_id = config.DEFAULT_USER_TEST_PROFILE
         workflow = WorkflowModel(
--- lunarbase/base/lunarbase/domains/workflow/repositories/tests/test_local_files_workflow_repository/test_tmp_save.py
+++ lunarbase/base/lunarbase/domains/workflow/repositories/tests/test_local_files_workflow_repository/test_tmp_save.py
@@ -6,19 +6,7 @@ from lunarbase.modeling.data_models import WorkflowModel
 import uuid
 
 
-class TestTmpSaveWorkflow:
-    def test_tmp_saves_workflow_returns_saved_workflow(self, workflow_repository, config):
-        user_id = config.DEFAULT_USER_TEST_PROFILE
-        workflow = WorkflowModel(
-            name="Workflow Name",
-            description="Workflow Description",
-            id=str(uuid.uuid4()),
-        )
-
-        saved_workflow = workflow_repository.tmp_save(user_id, workflow)
-
-        assert saved_workflow.id == workflow.id
-        
+class TestTmpSaveWorkflow:       
     def test_tmp_saves_workflow_in_correct_path(self, workflow_repository, config):
         user_id = config.DEFAULT_USER_TEST_PROFILE
         workflow = WorkflowModel(
