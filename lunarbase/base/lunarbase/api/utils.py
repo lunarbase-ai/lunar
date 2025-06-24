6d0225eb68a404784447acbc197756b3c9817851
gbmartins9 <gabrielmf@dcc.ufrj.br>
2025-06-24 16:27:42 -0300

    feat(api): add APIContextInitializer for unified context setup

--- lunarbase/base/lunarbase/api/utils.py
+++ lunarbase/base/lunarbase/api/utils.py
@@ -10,6 +10,15 @@ from pydantic import BaseModel
 
 from lunarbase.utils import setup_logger
 
+from lunarbase.api.component import ComponentAPI
+from lunarbase.api.workflow import WorkflowAPI
+from lunarbase.controllers.demo_controller import DemoController
+from lunarbase.controllers.report_controller import ReportController
+from lunarbase.controllers.file_controller import FileController
+from lunarbase.controllers.code_completion_controller import CodeCompletionController
+from lunarbase.controllers.datasource_controller import DatasourceController
+from lunarbase.controllers.llm_controller import LLMController
+
 API_LOGGER = setup_logger("lunarbase-api")
 
 
@@ -34,3 +43,34 @@ class TimedLoggedRoute(APIRoute):
             return response
 
         return timed_logged_route_handler
+
+
+class APIContextInitializer:
+    @staticmethod
+    def initialize_api_context(api_context):
+        api_context.component_api = ComponentAPI(api_context.lunar_config)
+        api_context.workflow_api = WorkflowAPI(api_context.lunar_config)
+        api_context.demo_controller = DemoController(api_context.lunar_config)
+        api_context.report_controller = ReportController(
+            api_context.lunar_config,
+            persistence_layer=api_context.lunar_registry.persistence_layer,
+        )
+        api_context.file_controller = FileController(
+            api_context.lunar_config,
+            persistence_layer=api_context.lunar_registry.persistence_layer,
+        )
+        api_context.code_completion_controller = CodeCompletionController(
+            api_context.lunar_config
+        )
+        api_context.datasource_controller = DatasourceController(
+            api_context.lunar_config,
+        )
+        api_context.llm_controller = LLMController(
+            api_context.lunar_config,
+        )
+        api_context.component_api.index_global()
+
+
+def initialize_api_context(api_context):
+    APIContextInitializer.initialize_api_context(api_context)
+    
